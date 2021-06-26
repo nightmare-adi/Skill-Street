837124bf2f8cc918c22893a508cd38c5883b512f
Adi <adigarg97@gmail.com>
2021-06-26 11:48:19 +0530

    setup

--- src/App.js
+++ src/App.js
@@ -1,28 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css';
-import React from 'react'
+import React, { Component } from 'react';
+import Contact from './Components/Contact';
+import Events from './Components/Events';
+import Home from './Components/Home';
+import Team from './Components/Team';
+import Winners from './Components/Winners';
+import {
+  Menu
+} from 'semantic-ui-react'
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeItem: "home"
+        }
+    }
+
+    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+    render() {
+    const { activeItem } = this.state
+
+  	    return (
+  	        <div>
+                <Menu>
+                    <Menu.Item
+                        name='home'
+                        active={activeItem === 'home'}
+                        onClick={this.handleItemClick}
+                    >Home</Menu.Item>
+
+                    <Menu.Item
+                        name='events'
+                        active={activeItem === 'events'}
+                        onClick={this.handleItemClick}
+                    >Events</Menu.Item>
+
+                    <Menu.Item
+                        name='winners'
+                        active={activeItem === 'winners'}
+                        onClick={this.handleItemClick}
+                    >Wall Of Fame</Menu.Item>
+
+                    <Menu.Item
+                        name='team'
+                        active={activeItem === 'team'}
+                        onClick={this.handleItemClick}
+                    >The Team</Menu.Item>
+
+                    <Menu.Item
+                        name='contact'
+                        active={activeItem === 'contact'}
+                        onClick={this.handleItemClick}
+                    >Contact Us</Menu.Item>
+                </Menu>
+
+                <div>
+                    {activeItem === 'home' && <Home />}
+                    {activeItem === 'events' && <Events />}
+                    {activeItem === 'winners' && <Winners />}
+                    {activeItem === 'team' && <Team />}
+                    {activeItem === 'contact' && <Contact />}
+                </div>
+            </div>
+  	    );
+  	}
 }
-
-export default App;
---
+++ src/Components/Winners.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Winners extends Component {
+
+    render() {
+
+  	    return (
+  	        <div>
+  	            Winners
+  	        </div>
+  	    );
+  	}
+}
