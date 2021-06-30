797244e97346bd4494ccfe18de3f1abbf20ac1c3
Adi <adigarg97@gmail.com>
2021-06-30 15:52:26 +0530

    setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
 import 'semantic-ui-css/semantic.min.css';
 import React, { Component } from 'react';
-import Contact from './Components/Contact';
 import Events from './Components/Events';
 import Home from './Components/Home';
 import Team from './Components/Team';
 import Winners from './Components/Winners';
 import {
-  Menu
-} from 'semantic-ui-react'
+  Menu,
+  Button,
+  Icon,
+  TransitionablePortal,
+  Segment,
+  Header
+} from 'semantic-ui-react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 export default class App extends Component {
@@ -16,9 +21,17 @@ export default class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            activeItem: "home"
+            activeItem: "home",
+            phonePopup: false,
+            emailPopup: false
         }
     }
+
+    handlePhoneOpen = () => this.setState({ phonePopup: true });
+    handleEmailOpen = () => this.setState({ phonePopup: true });
+
+    handlePhoneClose = () => this.setState({ emailPopup: false });
+    handleEmailClose = () => this.setState({ emailPopup: false });
 
     handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
@@ -27,44 +40,105 @@ export default class App extends Component {
 
   	    return (
   	        <div>
-                <Menu>
-                    <Menu.Item
-                        name='home'
-                        active={activeItem === 'home'}
-                        onClick={this.handleItemClick}
-                    >Home</Menu.Item>
+  	            <div className="navigation">
+                    <nav class="navbar navbar-expand navbar-dark bg-dark">
+                        <div class="container">
+                            <div class="navbar-brand">
+                                Skill Street
+                            </div>
+                            <div>
+                                <Menu secondary inverted>
+                                    <Menu.Item
+                                        name='home'
+                                        active={activeItem === 'home'}
+                                        onClick={this.handleItemClick}
+                                    >Home</Menu.Item>
 
-                    <Menu.Item
-                        name='events'
-                        active={activeItem === 'events'}
-                        onClick={this.handleItemClick}
-                    >Events</Menu.Item>
+                                    <Menu.Item
+                                        name='events'
+                                        active={activeItem === 'events'}
+                                        onClick={this.handleItemClick}
+                                    >Events</Menu.Item>
 
-                    <Menu.Item
-                        name='winners'
-                        active={activeItem === 'winners'}
-                        onClick={this.handleItemClick}
-                    >Wall Of Fame</Menu.Item>
+                                    <Menu.Item
+                                        name='winners'
+                                        active={activeItem === 'winners'}
+                                        onClick={this.handleItemClick}
+                                    >Wall Of Fame</Menu.Item>
 
-                    <Menu.Item
-                        name='team'
-                        active={activeItem === 'team'}
-                        onClick={this.handleItemClick}
-                    >The Team</Menu.Item>
+                                    <Menu.Item
+                                        name='team'
+                                        active={activeItem === 'team'}
+                                        onClick={this.handleItemClick}
+                                    >The Team</Menu.Item>
+                                </Menu>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
 
-                    <Menu.Item
-                        name='contact'
-                        active={activeItem === 'contact'}
-                        onClick={this.handleItemClick}
-                    >Contact Us</Menu.Item>
-                </Menu>
 
                 <div>
                     {activeItem === 'home' && <Home />}
                     {activeItem === 'events' && <Events />}
                     {activeItem === 'winners' && <Winners />}
                     {activeItem === 'team' && <Team />}
-                    {activeItem === 'contact' && <Contact />}
+                </div>
+
+                <div className="footer">
+                    <footer class="py-4 bg-dark fixed-bottom">
+                        <div class="container">
+                            <div class="text-center text-white pb-4">
+                                Contact Us
+                            </div>
+                            <div class="m-0 text-center text-white">
+                                <Button color='facebook'
+                                        onClick={() => window.open('https://www.facebook.com/theskillstreetofficial', '_blank')}>
+                                    <Icon name='facebook' /> Facebook
+                                </Button>
+
+                                <Button color='instagram'
+                                        onClick={() => window.open('https://www.instagram.com/skill_street', '_blank')}>
+                                    <Icon name='instagram' /> Instagram
+                                </Button>
+
+                                <TransitionablePortal
+                                    closeOnTriggerClick
+                                    onOpen={this.handlePhoneOpen}
+                                    onClose={this.handlePhoneClose}
+                                    openOnTriggerClick
+                                    trigger={
+                                        <Button color='green'>
+                                            <Icon name='whatsapp' /> Phone
+                                        </Button>
+                                    }
+                                >
+                                    <Segment style={{ left: '40%', position: 'fixed', top: '50%', zIndex: 1000 }}>
+                                        <Header>Our Contact Information</Header>
+                                        <p>Amita Garg - 9356200139</p>
+                                        <p>Aditya Garg - 9023300022</p>
+                                    </Segment>
+                                </TransitionablePortal>
+
+                                <TransitionablePortal
+                                    closeOnTriggerClick
+                                    onOpen={this.handleEmailOpen}
+                                    onClose={this.handleEmailClose}
+                                    openOnTriggerClick
+                                    trigger={
+                                        <Button color='red'>
+                                            <Icon name='mail outline' /> Email
+                                        </Button>
+                                    }
+                                >
+                                    <Segment style={{ left: '40%', position: 'fixed', top: '50%', zIndex: 1000 }}>
+                                        <Header>Our Email Id</Header>
+                                        <p>theskillstreetofficial@gmail.com</p>
+                                    </Segment>
+                                </TransitionablePortal>
+                            </div>
+                        </div>
+                    </footer>
                 </div>
             </div>
   	    );
